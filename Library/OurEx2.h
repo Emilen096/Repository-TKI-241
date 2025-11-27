5be58ca87ada51f546be5b86794bb07e7089c0e3
Emilen096 <emiltsvetkov07@gmail.com>
2025-11-27 12:53:31 +0300

    OurEx2.h

--- Library/OurEx2.h
+++ Library/OurEx2.h
@@ -8,11 +8,7 @@ class OurEx2 : public Exercise {
 public:
     using Exercise::Exercise;
 
-    void Task2(int K) override; // Только Task2
-    
-    // Пустые реализации для остальных задач
-    void Task1() override {}
-    Matrix<int> Task3() const override { return Matrix<int>{}; }
+    void Task() override;
 };
 
 } // namespace algebra
