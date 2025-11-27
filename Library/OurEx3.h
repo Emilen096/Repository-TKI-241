a97439358f67f6e8260c2a6bc59c447471cd066e
Emilen096 <emiltsvetkov07@gmail.com>
2025-11-27 12:55:11 +0300

    OurEx3.h

--- Library/OurEx3.h
+++ Library/OurEx3.h
@@ -8,11 +8,7 @@ class OurEx3 : public Exercise {
 public:
     using Exercise::Exercise;
 
-    Matrix<int> Task3() const override; // Только Task3
-    
-    // Пустые реализации для остальных задач
-    void Task1() override {}
-    void Task2(int K) override {}
+    void Task() override;
 };
 
 } // namespace algebra
