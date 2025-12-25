0b0f5ab3911f23194cf316b81532963f848551fc
Emilen096 <emiltsvetkov07@gmail.com>
2025-12-25 10:38:39 +0300

    Update Exercise.h

--- Library/Exercise.h
+++ Library/Exercise.h
@@ -1,36 +1,34 @@
 #pragma once
-#include "Matrix.h" // Подключаем Matrix, так как теперь fill и конструктор находятся в Matrix
+#include "Matrix.h"
+#include "Generator.h"
 #include <memory>
 
 namespace miit {
 namespace algebra {
 
-// Exercise теперь НЕ содержит перемещённых строк
 class Exercise {
 protected:
-    Matrix<int> data_; // Используем Matrix<int> (предполагаем, что Matrix теперь может хранить int и имеет генератор)
-    // std::unique_ptr<Generator> gen_; // <-- УБРАНО, теперь в Matrix
+    Matrix<int> data_;
 
 public:
-    // Exercise больше не принимает gen в конструкторе, так как gen теперь в Matrix
-    // explicit Exercise(std::unique_ptr<Generator> gen); // <-- УБРАНО
-    // virtual ~Exercise() = default; // <-- УБРАНО
-    // void fill(std::size_t n); // <-- УБРАНО
-
-    // Если Exercise всё ещё нужен, можно добавить конструктор, который инициализирует Matrix с генератором
     explicit Exercise(std::unique_ptr<Generator> gen);
 
-    // Виртуальные методы задач
+    virtual ~Exercise() = default;
+
+    void fill(std::size_t n);
+
+    enum class InputType {
+        Random = 1,
+        Constant = 2,
+        Manual = 3
+    };
+
     virtual void Task1() = 0;
     virtual void Task2(int K) = 0;
     virtual Matrix<int> Task3() const = 0;
 
     const Matrix<int>& data() const noexcept { return data_; }
 };
-
-// Реализация конструктора Exercise
-template<typename T>
-Exercise::Exercise(std::unique_ptr<Generator> gen) : data_(std::move(gen)) {} // Используем новый конструктор Matrix
 
 } // namespace algebra
 } // namespace miit
