ce9749a9bcb31883c1b90a9c2fcb2b0abc794bcb
Emilen096 <emiltsvetkov07@gmail.com>
2025-12-25 10:38:01 +0300

    Update Matrix.h

--- Library/Matrix.h
+++ Library/Matrix.h
@@ -1,17 +1,14 @@
 #pragma once
-
 #include <cstddef>
 #include <string>
-#include <vector>
-#include <memory> // Для std::unique_ptr
-#include <iostream> // Для operator<<, если используется здесь
-#include <sstream>  // Для to_string
-
-// Подключаем Generator, так как Matrix теперь зависит от него
-#include "Generator.h"
+#include <ostream>
+#include <initializer_list>
+#include <memory>
 
 namespace miit {
 namespace algebra {
+
+class Generator; // Forward declaration
 
 template<typename T>
 class Matrix {
@@ -19,152 +16,57 @@ private:
     T* data_ = nullptr;
     std::size_t size_ = 0;
     std::size_t capacity_ = 0;
-    std::unique_ptr<Generator> gen_; // Храним генератор
+    std::unique_ptr<Generator> gen_;
 
     void reallocate(std::size_t new_capacity);
 
 public:
-    // --- Перемещённые строки из Exercise.h ---
     explicit Matrix(std::unique_ptr<Generator> gen);
-    virtual ~Matrix() = default; // Виртуальный деструктор
-    void fill(std::size_t n); // Метод fill
-    // --- Конец перемещённых строк ---
+    virtual ~Matrix() = default;
+    void fill(std::size_t n);
 
-    // --- Остальные конструкторы ---
     Matrix();
     explicit Matrix(std::size_t n, const T& value = T{});
-    // ... (остальные конструкторы, если были)
+    Matrix(std::initializer_list<T> init);
+    Matrix(const Matrix& other);
+    Matrix(Matrix&& other) noexcept;
 
-    // --- Остальные методы ---
+    ~Matrix();
+
+    Matrix& operator=(const Matrix& other);
+    Matrix& operator=(Matrix&& other) noexcept;
+
     T& operator[](std::size_t index);
     const T& operator[](std::size_t index) const;
+
     std::size_t size() const noexcept;
     bool empty() const noexcept;
+    T& front();
+    const T& front() const;
+    T& back();
+    const T& back() const;
+
     void push_back(const T& value);
     void push_back(T&& value);
     void pop_back();
-    // ... (остальные методы)
+    void push_front(const T& value);
+    void push_front(T&& value);
+    void pop_front();
+    void insert(std::size_t index, const T& value);
+    void erase(std::size_t index);
+    void clear();
 
-    // --- Вывод в строку ---
+    Matrix& operator<<(const T& value);
+    Matrix& operator>>(T& value);
+
     std::string to_string() const;
 
-    // --- Дружественный оператор вывода ---
     friend std::ostream& operator<<(std::ostream& os, const Matrix& m) {
         return os << m.to_string();
     }
 };
 
-// --- Реализация методов шаблона ---
-template<typename T>
-void Matrix<T>::reallocate(std::size_t new_capacity) {
-    T* new_data = new T[new_capacity];
-    std::size_t copy_size = std::min(size_, new_capacity);
-    for (std::size_t i = 0; i < copy_size; ++i) {
-        new_data[i] = std::move_if_noexcept(data_[i]);
-    }
-    delete[] data_;
-    data_ = new_data;
-    capacity_ = new_capacity;
-    if (size_ > new_capacity) size_ = new_capacity;
-}
-
-template<typename T>
-Matrix<T>::Matrix() : data_(nullptr), size_(0), capacity_(0), gen_(nullptr) {}
-
-template<typename T>
-Matrix<T>::Matrix(std::size_t n, const T& value) : size_(n), capacity_(n), gen_(nullptr) {
-    if (n > 0) {
-        data_ = new T[n];
-        for (std::size_t i = 0; i < n; ++i) {
-            data_[i] = value;
-        }
-    }
-}
-
-// --- Реализация перемещённых методов ---
-template<typename T>
-Matrix<T>::Matrix(std::unique_ptr<Generator> gen) : data_(nullptr), size_(0), capacity_(0), gen_(std::move(gen)) {}
-
-template<typename T>
-void Matrix<T>::fill(std::size_t n) {
-    this->clear(); // Очищаем текущие данные
-    for (std::size_t i = 0; i < n; ++i) {
-        this->push_back(static_cast<T>(gen_->generate())); // Предполагаем, что generate() возвращает int
-    }
-}
-
-// --- Остальные реализации методов ---
-template<typename T>
-T& Matrix<T>::operator[](std::size_t index) {
-    if (index >= size_) {
-        throw std::out_of_range("Matrix::operator[]: index out of range");
-    }
-    return data_[index];
-}
-
-template<typename T>
-const T& Matrix<T>::operator[](std::size_t index) const {
-    if (index >= size_) {
-        throw std::out_of_range("Matrix::operator[]: index out of range");
-    }
-    return data_[index];
-}
-
-template<typename T>
-std::size_t Matrix<T>::size() const noexcept {
-    return size_;
-}
-
-template<typename T>
-bool Matrix<T>::empty() const noexcept {
-    return size_ == 0;
-}
-
-template<typename T>
-void Matrix<T>::push_back(const T& value) {
-    if (size_ >= capacity_) {
-        std::size_t new_capacity = (capacity_ == 0) ? 1 : capacity_ * 2;
-        reallocate(new_capacity);
-    }
-    data_[size_++] = value;
-}
-
-template<typename T>
-void Matrix<T>::push_back(T&& value) {
-    if (size_ >= capacity_) {
-        std::size_t new_capacity = (capacity_ == 0) ? 1 : capacity_ * 2;
-        reallocate(new_capacity);
-    }
-    data_[size_++] = std::move(value);
-}
-
-template<typename T>
-void Matrix<T>::pop_back() {
-    if (empty()) {
-        throw std::runtime_error("Matrix::pop_back(): called on empty matrix");
-    }
-    --size_;
-}
-
-template<typename T>
-void Matrix<T>::clear() {
-    size_ = 0;
-}
-
-template<typename T>
-std::string Matrix<T>::to_string() const {
-    std::ostringstream oss;
-    oss << "[ ";
-    for (std::size_t i = 0; i < size_; ++i) {
-        if (i > 0) oss << ", ";
-        oss << data_[i];
-    }
-    oss << " ]";
-    return oss.str();
-}
-
-// --- Явная инстанциация для int ---
-template class Matrix<int>;
+extern template class Matrix<int>;
 
 } // namespace algebra
 } // namespace miit
