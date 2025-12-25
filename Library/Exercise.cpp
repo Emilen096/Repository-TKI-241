543f814707b4d8572d1dfdf11326decfefafc46b
Emilen096 <emiltsvetkov07@gmail.com>
2025-12-25 10:38:56 +0300

    Update Exercise.cpp

--- Library/Exercise.cpp
+++ Library/Exercise.cpp
@@ -1,15 +1,14 @@
 #include "Exercise.h"
 
-// --- Используем старый синтаксис namespace ---
 namespace miit {
 namespace algebra {
 
-Exercise::Exercise(std::unique_ptr<Generator> gen) : gen_(std::move(gen)) {}
+Exercise::Exercise(std::unique_ptr<Generator> gen) : data_(std::move(gen)) {}
 
 void Exercise::fill(std::size_t n) {
     data_.clear();
     for (std::size_t i = 0; i < n; ++i) {
-        data_ << gen_->generate(); // Используем operator<< для Matrix
+        data_ << data_.gen_->generate();
     }
 }
 
